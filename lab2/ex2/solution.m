20018086a25b1216ef3cb5e8798b3e747e94f144
Davide Pedranz <davide.pedranz@gmail.com>
2017-11-27 10:12:00 +0100

    Lab 2 - Ex 2

---
+++ lab2/ex2/solution.m
@@ -0,0 +1,30 @@
+clear;
+
+% symbolic computation
+syms a b c d k;
+
+% data
+X = [a b ; c d];
+dim = size(X, 1);
+
+% question 1
+m = mean(X);
+covariance = (X - m)' * (X - m) / dim;
+disp(covariance);
+
+% question 2
+X1 = X + k;
+m1 = mean(X1);
+disp('X - m');
+disp(X - m);
+disp('X1 - m1');
+disp(X1 - m1);
+disp('Covariance of X + k does is equal to the one of k');
+
+% question 3
+X2 = X * k;
+m2 = mean(X2);
+disp('X2 - m2');
+disp(X2 - m2);
+disp('Covariance of X * k does is multiplied by k^2 with respect to X');
+
