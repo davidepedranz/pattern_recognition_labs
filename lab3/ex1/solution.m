df46210e75648f7e4bb4cb7803ce8611822cfa2c
Davide Pedranz <davide.pedranz@gmail.com>
2017-12-10 18:56:20 +0100

    Lab 3 - Ex 1: ROC

--- lab3/ex1/solution.m
+++ lab3/ex1/solution.m
@@ -1,4 +1,5 @@
-clf; close; close; clear;
+clf; close; close; 
+clear;
 format long;
 
 % input
@@ -9,19 +10,19 @@ threshold = 10;
 
 % question 1: P(hit)
 p_hit = 1 - normcdf(threshold, mu_2, sigma);
-fprintf("p_hit = %.6f\n", p_hit);
+fprintf('p_hit = %.6f\n', p_hit);
 
 % question 2: P(false alarm)
 p_fa = 1 - normcdf(threshold, mu_1, sigma);
-fprintf("p_fa = %.6f\n", p_fa);
+fprintf('p_fa = %.6f\n', p_fa);
 
 % question 3,4: discriminability
-d1 = abs(mu_2 - mu_1) / sigma;
-fprintf("discriminability (3) = %.6f\n", d1);
-d2 = abs(9 - 4) / sigma;
-fprintf("discriminability (4) = %.6f\n", d2);
+d1 = discriminability(mu_1, mu_2, sigma);
+fprintf('discriminability (3) = %.6f\n', d1);
+d2 = discriminability(5, 9, sqrt(4));
+fprintf('discriminability (4) = %.6f\n', d2);
 q5 = 3 * sigma + 5;
-fprintf("mu_2 (5) = %.6f\n", q5);
+fprintf('mu_2 (5) = %.6f\n', q5);
 
 % question 7-10
 data = importdata('data/lab3_1.mat');
@@ -31,6 +32,30 @@ fp = sum(ismember(data, [0 1], 'rows'));
 tn = sum(ismember(data, [0 0], 'rows'));
 hit_rate = tp / (tp + fn);
 fa_rate = 1 - (tn / (fp + tn));
-fprintf("hit_rate = %.6f\n", hit_rate);
-fprintf("fa_rate = %.6f\n", fa_rate);
+fprintf('hit_rate = %.6f\n', hit_rate);
+fprintf('fa_rate = %.6f\n', fa_rate);
 
+% ROC curve
+my_sigma = 0.94163665;
+x = 0:0.1:20;
+false_positive_rate = 1 - normcdf(x, mu_1, my_sigma);
+true_positive_rate = 1 - normcdf(x, mu_2, my_sigma);
+figure;
+plot(fa_rate, hit_rate, 'ro', 'markers', 4);
+title(sprintf('ROC curve for \\mu_1=%d, \\mu_2=%d, \\sigma=%.6f', mu_1, mu_2, my_sigma));
+xlabel('False Positive Rate');
+ylabel('True Positive Rate');
+pbaspect([1 1 1]);
+hold on;
+plot(false_positive_rate, true_positive_rate);
+hold off;
+% xlim([0.099999, 0.100001]);
+% ylim([0.799999, 0.800001]);
+saveas(gcf, 'roc', 'png');
+
+roc_discriminability = discriminability(mu_1, mu_2, my_sigma);
+fprintf('roc_discriminability = %.6f\n', roc_discriminability);
+
+function value = discriminability(mu_1, mu_2, sigma) 
+    value = abs(mu_2 - mu_1) / sigma;
+end
