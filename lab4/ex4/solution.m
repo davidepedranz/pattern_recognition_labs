c6a3b585dfcc7adaac92f943201df4642a801f65
Davide Pedranz <davide.pedranz@gmail.com>
2017-12-11 16:55:45 +0100

    Lab 4 - Ex 4

---
+++ lab4/ex4/solution.m
@@ -0,0 +1,25 @@
+clf; close all;
+clear;
+
+% load the image
+tool_uint8 = imread('data/HeadTool0002.bmp');
+tool_raw = im2double(tool_uint8);
+
+% enhance image
+tool = adapthisteq(tool_raw);
+
+% look for circles
+[centers, radii] = imfindcircles(tool, [20, 40], 'Method', 'TwoStage', 'Sensitivity', 0.9);
+
+% plot the circles
+figure;
+imshow(tool);
+viscircles(centers, radii, 'EdgeColor', 'g');
+saveas(gcf, 'all_circles', 'png');
+
+% plot the top 2 circles
+% NB: the outputs are already ordered by metric decrescent
+figure;
+imshow(tool);
+viscircles(centers(1:2, :), radii(1:2), 'EdgeColor', 'r');
+saveas(gcf, 'top_circles', 'png');
