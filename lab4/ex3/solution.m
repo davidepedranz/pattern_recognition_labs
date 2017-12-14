30a0b21b5e4480cb28ac76788de073557565ff25
Davide Pedranz <davide.pedranz@gmail.com>
2017-12-14 17:33:00 +0100

    Lab 4 - Ex 3: cleanup

--- lab4/ex3/solution.m
+++ lab4/ex3/solution.m
@@ -1,6 +1,9 @@
 clf; close all;
 clear;
 
+% load the function myhoughline of exercise 1
+addpath(genpath('../ex1'));
+
 % load the image
 chess_rgb = imread('data/chess.jpg');
 chess = rgb2gray(chess_rgb);
@@ -11,24 +14,15 @@ edges = edge(chess, 'canny');
 % hough transform
 [accumulator, theta, rho] = hough(edges);
 
-% load myhoughline
-addpath(genpath('../ex1'));
+% find the maximum peak and show it in Hough space
+peak = houghpeaks(accumulator, 1);
+hough_plot(peak, accumulator, theta, rho);
+saveas(gcf, 'top_peak', 'png');
 
-% find the peaks
+% find the top 15 peaks and show them in Hough space
 peaks = houghpeaks(accumulator, 15);
-
-% show the plot of Hough space
-figure;
-imshow(imadjust(rescale(accumulator)), 'XData', theta, 'YData', rho, 'InitialMagnification', 'fit');
-title('Hough transform of chess.jpg with peaks');
-xlabel('\theta');
-ylabel('\rho');
-axis on;
-axis normal;
-hold on;
-plot(theta(peaks(:, 2)), rho(peaks(:, 1)), 's', 'color', 'red');
-hold off;
-saveas(gcf, 'peaks', 'png');
+hough_plot(peaks, accumulator, theta, rho);
+saveas(gcf, 'top_15_peaks', 'png');
 
 % image of chess + lines
 figure;
@@ -59,3 +53,16 @@ for k = 1:length(peaks)
     end
 end
 saveas(gcf, 'comparison', 'png');
+
+function hough_plot(peaks, accumulator, theta, rho)
+    figure;
+    imshow(imadjust(rescale(accumulator)), 'XData', theta, 'YData', rho, 'InitialMagnification', 'fit');
+    title('Hough transform of chess.jpg with peaks');
+    xlabel('\theta');
+    ylabel('\rho');
+    axis on;
+    axis normal;
+    hold on;
+    plot(theta(peaks(:, 2)), rho(peaks(:, 1)), 's', 'color', 'red');
+    hold off;
+end
